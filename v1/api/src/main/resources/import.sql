28fd99cd214540aa7e8c1013ad4e74704f94e931
Miha Jamsek <miha_jamsek@windowslive.com>
2022-03-09 19:17:50 +0100

    removed old data

--- v1/api/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO sys_roles(id, created_at, updated_at, role_id, name) VALUES ('1dfa6c22-1ca4-4b4b-820c-e90fcb84cd71', NOW(), NOW(), 'admin', 'Admin');
-INSERT INTO sys_roles(id, created_at, updated_at, role_id, name) VALUES ('73652b3b-6dbb-455d-85f6-6284556abae6', NOW(), NOW(), 'user', 'User');
-
