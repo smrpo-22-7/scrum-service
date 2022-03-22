e726aec64cd87f20ccfe8bcf60af971392dfc352
ad0488 <ad0488@student.uni-lj.si>
2022-03-22 13:03:38 +0100

    Story BE 3. commit

---
+++ v1/lib/src/main/java/si/smrpo/scrum/lib/stories/AcceptanceTest.java
@@ -0,0 +1,27 @@
+package si.smrpo.scrum.lib.stories;
+
+import si.smrpo.scrum.lib.stories.*;
+
+public class AcceptanceTest {
+
+    private String result;
+
+    private Story story;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Story getStory() {
+        return story;
+    }
+
+    public void setStory(Story story) {
+        this.story = story;
+    }
+
+}
--- v1/lib/src/main/java/si/smrpo/scrum/lib/stories/Story.java
+++ v1/lib/src/main/java/si/smrpo/scrum/lib/stories/Story.java
@@ -1,4 +1,69 @@
 package si.smrpo.scrum.lib.stories;
 
+import si.smrpo.scrum.lib.enums.SimpleStatus;
+import si.smrpo.scrum.lib.enums.StoryPriority;
+
+import java.util.List;
+
 public class Story {
+
+    private String title;
+
+    private String description;
+
+    private SimpleStatus status;
+
+    private Integer businessValue;
+
+    private StoryPriority priority;
+
+    private List<AcceptanceTest> tests;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public SimpleStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SimpleStatus status) {
+        this.status = status;
+    }
+
+    public Integer getBusinessValue() {
+        return businessValue;
+    }
+
+    public void setBusinessValue(int businessValue) {
+        this.businessValue = businessValue;
+    }
+
+    public StoryPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(StoryPriority priority) {
+        this.priority = priority;
+    }
+
+    public List<AcceptanceTest> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<AcceptanceTest> tests) {
+        this.tests = tests;
+    }
 }
